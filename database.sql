d3189b69c618c80d1158c754843a173159b5e793
mawei <69443245@qq.com>
2016-05-12 23:14:42 +0800

    sql

--- database.sql
+++ database.sql
@@ -2,8 +2,7 @@
 SQLyog Ultimate v12.09 (64 bit)
 MySQL - 5.6.10 : Database - l1
 *********************************************************************
-*/
-
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -113,11 +112,11 @@ CREATE TABLE `tb_dic` (
   `valueField` int(10) NOT NULL,
   `ord` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 /*Data for the table `tb_dic` */
 
-insert  into `tb_dic`(`id`,`keyWord`,`textField`,`valueField`,`ord`) values (1,'warehouseState','停用',0,0),(2,'warehouseState','使用',1,1),(3,'rentStatus','已取消',-1,1),(4,'rentStatus','已下单,未支付',1,2),(5,'rentStatus','已支付',2,3),(6,'rentStatus','已发货',3,4),(7,'rentStatus','已收货,待退还',4,5),(8,'rentStatus','已退还,待收货',5,6),(9,'rentStatus','已完成',6,7);
+insert  into `tb_dic`(`id`,`keyWord`,`textField`,`valueField`,`ord`) values (1,'warehouseState','停用',0,0),(2,'warehouseState','使用',1,1),(3,'rentStatus','已取消',-1,1),(4,'rentStatus','已下单,未支付',0,2),(5,'rentStatus','已支付',1,3),(6,'rentStatus','已发货',2,4),(7,'rentStatus','已收货,待退还',3,5),(8,'rentStatus','已退还,待收货',4,6),(9,'rentStatus','已完成',5,7),(10,'rentBillStatus','未审核',0,0),(11,'rentBillStatus','已审核',1,1),(12,'rentBillStatus','已归还',2,2),(13,'itemStatus','停用',0,0),(14,'itemStatus','使用',1,1),(15,'sizeStatus','停用',0,0),(16,'sizeStatus','使用',1,1),(17,'purchaseStatus','未审核',0,0),(18,'purchaseStatus','已审核',1,1);
 
 /*Table structure for table `tb_inventory` */
 
@@ -150,7 +149,7 @@ CREATE TABLE `tb_item` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   `code` varchar(20) DEFAULT NULL,
-  `stat` varchar(10) DEFAULT '使用',
+  `stat` varchar(10) DEFAULT '1',
   `create_by` bigint(20) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_by` bigint(20) DEFAULT NULL,
@@ -170,11 +169,11 @@ CREATE TABLE `tb_item` (
   `size` varchar(250) DEFAULT NULL,
   `sizeName` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 /*Data for the table `tb_item` */
 
-insert  into `tb_item`(`id`,`name`,`code`,`stat`,`create_by`,`create_time`,`update_by`,`update_time`,`brandId`,`color`,`brandName`,`primaryCategoryId`,`minorCategoryId`,`primaryCategoryName`,`minorCategoryName`,`rental1`,`rental2`,`deposit`,`style`,`colorname`,`size`,`sizeName`) values (4,'西服','xifu','使用',NULL,NULL,NULL,'2016-04-14 10:31:36',5,'6,7','Adidas',4,5,'西裤','西裤22','29.00','1.00','1.00','kuanhao','白,绿色',NULL,NULL),(5,'sss','sa','使用',NULL,'2016-04-18 22:00:51',NULL,'2016-04-19 13:07:39',4,'6','Nike',4,4,'西裤','牛仔裤11','11.00','11.00','11.00','33','白','8,11','165S,175XX'),(6,'xxx','xx','使用',NULL,'2016-04-19 00:23:48',NULL,NULL,5,'6,7','Adidas',4,4,'西裤','牛仔裤11','12.00','1222.00','112.00',NULL,'白,绿色','8,10,11','165S,180L,175XX'),(7,'gg','gg','使用',NULL,'2016-04-19 00:28:45',NULL,'2016-04-19 13:06:15',4,'6,7','Nike',4,5,'西裤','西裤22','33.00','33.00','322.00','gggggg','白,绿色','8,10,11','165S,180L,175XX'),(8,'234','234','停用',NULL,'2016-05-08 11:31:20',NULL,'2016-05-08 11:31:29',4,'6','Nike',4,5,'西裤34234','西裤22','234.00','234.00','234.00','234','白','8','165S'),(9,'123','213','使用',NULL,'2016-05-09 18:38:48',NULL,NULL,NULL,'6,7','',4,4,'西裤34234','牛仔裤11','123.00','123.00','123.00','123','白,绿色','8,9','165S,170');
+insert  into `tb_item`(`id`,`name`,`code`,`stat`,`create_by`,`create_time`,`update_by`,`update_time`,`brandId`,`color`,`brandName`,`primaryCategoryId`,`minorCategoryId`,`primaryCategoryName`,`minorCategoryName`,`rental1`,`rental2`,`deposit`,`style`,`colorname`,`size`,`sizeName`) values (4,'西服','xifu','1',NULL,NULL,NULL,'2016-04-14 10:31:36',5,'6,7','Adidas',4,5,'西裤','西裤22','29.00','1.00','1.00','kuanhao','白,绿色',NULL,NULL),(5,'sss','sa','0',NULL,'2016-04-18 22:00:51',NULL,'2016-04-19 13:07:39',4,'6','Nike',4,4,'西裤','牛仔裤11','11.00','11.00','11.00','33','白','8,11','165S,175XX'),(6,'xxx','xx','1',NULL,'2016-04-19 00:23:48',NULL,NULL,5,'6,7','Adidas',4,4,'西裤','牛仔裤11','12.00','1222.00','112.00',NULL,'白,绿色','8,10,11','165S,180L,175XX'),(7,'gg','gg','1',NULL,'2016-04-19 00:28:45',NULL,'2016-04-19 13:06:15',4,'6,7','Nike',4,5,'西裤','西裤22','33.00','33.00','322.00','gggggg','白,绿色','8,10,11','165S,180L,175XX'),(8,'234','234','1',NULL,'2016-05-08 11:31:20',NULL,'2016-05-08 11:31:29',4,'6','Nike',4,5,'西裤34234','西裤22','234.00','234.00','234.00','234','白','8','165S'),(9,'123','213','0',NULL,'2016-05-09 18:38:48',NULL,NULL,NULL,'6,7','',4,4,'西裤34234','牛仔裤11','123.00','123.00','123.00','123','白,绿色','8,9','165S,170'),(10,'123','123','使用',NULL,'2016-05-12 09:27:36',NULL,NULL,4,'6,7','Nike',6,4,'123','牛仔裤11','123.00','123.00','123.00','132','白,绿色','12,13','size1,size2');
 
 /*Table structure for table `tb_kctz` */
 
@@ -320,6 +319,7 @@ CREATE TABLE `tb_rent` (
   `customerName` varchar(50) DEFAULT NULL,
   `warehouseId` bigint(20) DEFAULT NULL,
   `stat` int(11) DEFAULT '0',
+  `billStat` tinyint(1) DEFAULT '0',
   `create_by` bigint(20) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_by` bigint(20) DEFAULT NULL,
@@ -335,14 +335,12 @@ CREATE TABLE `tb_rent` (
   `returnBillNo` varchar(50) DEFAULT NULL,
   `beginDate` date DEFAULT NULL,
   `endDate` date DEFAULT NULL,
-  `warehouseName` varchar(50) DEFAULT NULL,
-  `billStatus` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=99 DEFAULT CHARSET=utf8;
 
 /*Data for the table `tb_rent` */
 
-insert  into `tb_rent`(`id`,`billNo`,`customerName`,`warehouseId`,`stat`,`create_by`,`create_time`,`update_by`,`update_time`,`rentDay`,`rentMoney`,`repoMoney`,`customerPhone`,`customerAddr`,`customerCard`,`supplierId`,`expressBillNo`,`returnBillNo`,`beginDate`,`endDate`,`warehouseName`,`billStatus`) values (75,'CZ201605090029',NULL,NULL,6,NULL,'2016-05-09 23:36:36',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0),(76,'CZ201605090030',NULL,NULL,6,NULL,'2016-05-09 23:37:50',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0);
+insert  into `tb_rent`(`id`,`billNo`,`customerName`,`warehouseId`,`stat`,`billStat`,`create_by`,`create_time`,`update_by`,`update_time`,`rentDay`,`rentMoney`,`repoMoney`,`customerPhone`,`customerAddr`,`customerCard`,`supplierId`,`expressBillNo`,`returnBillNo`,`beginDate`,`endDate`) values (98,'CZ2016000003','123',1,1,1,NULL,'2016-05-12 19:08:15',NULL,NULL,15,'45387.00','369.00','123','123','123','15','123','123','2016-05-04','2016-05-06');
 
 /*Table structure for table `tb_rentdtl` */
 
@@ -361,11 +359,11 @@ CREATE TABLE `tb_rentdtl` (
   `stat` bigint(20) DEFAULT NULL,
   `statName` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`dtlid`)
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8;
 
 /*Data for the table `tb_rentdtl` */
 
-insert  into `tb_rentdtl`(`dtlid`,`id`,`skuId`,`itemName`,`itemPrice`,`itemAmount`,`itemRepo`,`itemRent`,`warehouseId`,`stat`,`statName`) values (27,34,16,'234','234.00',324,'234.00','75816.00',NULL,-1,NULL),(28,34,16,'213','123.00',123,'123.00','15129.00',NULL,1,NULL),(29,35,16,'123','123.00',123,'123.00','15129.00',NULL,1,NULL),(30,36,15,'123','123.00',123,'123.00','15129.00',NULL,0,NULL),(31,37,16,'123','123.00',123,NULL,'15129.00',NULL,0,NULL),(36,38,16,'123','123.00',123,NULL,'15129.00',NULL,0,NULL),(37,38,15,'123','123.00',123,'123.00','15129.00',NULL,0,NULL);
+insert  into `tb_rentdtl`(`dtlid`,`id`,`skuId`,`itemName`,`itemPrice`,`itemAmount`,`itemRepo`,`itemRent`,`warehouseId`,`stat`,`statName`) values (44,98,28,'123','123.00',123,'123.00','15129.00',NULL,0,NULL),(45,98,29,'123','123.00',123,'123.00','15129.00',NULL,NULL,NULL),(46,98,30,'123','123.00',123,'123.00','15129.00',NULL,NULL,NULL);
 
 /*Table structure for table `tb_return` */
 
@@ -378,7 +376,7 @@ CREATE TABLE `tb_return` (
   `customerPhone` varchar(20) DEFAULT NULL,
   `customerAddr` varchar(100) DEFAULT NULL,
   `customerCard` varchar(50) DEFAULT NULL,
-  `supplierId` varchar(50) DEFAULT NULL,
+  `logisticsCompany` varchar(50) DEFAULT NULL,
   `expressBillNo` varchar(50) DEFAULT NULL,
   `rentBillNo` varchar(50) DEFAULT NULL,
   `warehouseId` bigint(20) DEFAULT NULL,
@@ -400,7 +398,7 @@ CREATE TABLE `tb_return` (
 
 /*Data for the table `tb_return` */
 
-insert  into `tb_return`(`id`,`billNo`,`customerName`,`customerPhone`,`customerAddr`,`customerCard`,`supplierId`,`expressBillNo`,`rentBillNo`,`warehouseId`,`warehouseName`,`stat`,`create_by`,`create_time`,`update_by`,`update_time`,`rentDay`,`rentMoney`,`repoMoney`,`damageMoney`,`beginDate`,`endDate`,`statName`) values (1,'3344','所发生','22','第三方','345566','22 第三方','的4444','222',2,'浦西仓库',1,NULL,'2016-04-27 23:17:32',NULL,'2016-05-09 22:42:51',8,'33.00','33.00','333.00','2016-04-20','2016-04-27','待审批'),(2,'','','','','','','','',NULL,NULL,1,NULL,'2016-05-09 22:41:50',NULL,'2016-05-09 22:42:42',NULL,NULL,NULL,NULL,NULL,NULL,'待审批'),(3,'','','','','','','','',NULL,NULL,1,NULL,'2016-05-09 22:42:37',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'待审批');
+insert  into `tb_return`(`id`,`billNo`,`customerName`,`customerPhone`,`customerAddr`,`customerCard`,`logisticsCompany`,`expressBillNo`,`rentBillNo`,`warehouseId`,`warehouseName`,`stat`,`create_by`,`create_time`,`update_by`,`update_time`,`rentDay`,`rentMoney`,`repoMoney`,`damageMoney`,`beginDate`,`endDate`,`statName`) values (1,'3344','所发生','22','第三方','345566','22 第三方','的4444','222',2,'浦西仓库',1,NULL,'2016-04-27 23:17:32',NULL,'2016-05-09 22:42:51',8,'33.00','33.00','333.00','2016-04-20','2016-04-27','待审批'),(2,'','','','','','','','',NULL,NULL,1,NULL,'2016-05-09 22:41:50',NULL,'2016-05-09 22:42:42',NULL,NULL,NULL,NULL,NULL,NULL,'待审批'),(3,'','','','','','','','',NULL,NULL,1,NULL,'2016-05-09 22:42:37',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'待审批');
 
 /*Table structure for table `tb_returndtl` */
 
@@ -437,11 +435,11 @@ CREATE TABLE `tb_seq` (
   `day` date NOT NULL,
   `seq` bigint(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8;
 
 /*Data for the table `tb_seq` */
 
-insert  into `tb_seq`(`id`,`prefix`,`day`,`seq`) values (14,'CZ','2016-05-09',1),(15,'CZ','2016-05-09',2),(16,'CZ','2016-05-09',3),(17,'CZ','2016-05-09',4),(18,'CZ','2016-05-09',5),(19,'CZ','2016-05-09',6),(20,'CZ','2016-05-09',7),(21,'CZ','2016-05-09',8),(22,'CZ','2016-05-09',9),(23,'CZ','2016-05-09',10),(24,'CZ','2016-05-09',11),(25,'CZ','2016-05-09',12),(26,'CZ','2016-05-09',13),(27,'CZ','2016-05-09',14),(28,'CZ','2016-05-09',15),(29,'CZ','2016-05-09',16),(30,'CZ','2016-05-09',17),(31,'CZ','2016-05-09',18),(32,'CZ','2016-05-09',19),(33,'CZ','2016-05-09',20),(34,'CZ','2016-05-09',21),(35,'CZ','2016-05-09',22),(36,'CZ','2016-05-09',23),(37,'CZ','2016-05-09',24),(38,'CZ','2016-05-09',25),(39,'CZ','2016-05-09',26),(40,'CZ','2016-05-09',27),(41,'CZ','2016-05-09',28),(42,'CZ','2016-05-09',29),(43,'CZ','2016-05-09',30);
+insert  into `tb_seq`(`id`,`prefix`,`day`,`seq`) values (14,'CZ','2016-05-09',1),(15,'CZ','2016-05-09',2),(16,'CZ','2016-05-09',3),(17,'CZ','2016-05-09',4),(18,'CZ','2016-05-09',5),(19,'CZ','2016-05-09',6),(20,'CZ','2016-05-09',7),(21,'CZ','2016-05-09',8),(22,'CZ','2016-05-09',9),(23,'CZ','2016-05-09',10),(24,'CZ','2016-05-09',11),(25,'CZ','2016-05-09',12),(26,'CZ','2016-05-09',13),(27,'CZ','2016-05-09',14),(28,'CZ','2016-05-09',15),(29,'CZ','2016-05-09',16),(30,'CZ','2016-05-09',17),(31,'CZ','2016-05-09',18),(32,'CZ','2016-05-09',19),(33,'CZ','2016-05-09',20),(34,'CZ','2016-05-09',21),(35,'CZ','2016-05-09',22),(36,'CZ','2016-05-09',23),(37,'CZ','2016-05-09',24),(38,'CZ','2016-05-09',25),(39,'CZ','2016-05-09',26),(40,'CZ','2016-05-09',27),(41,'CZ','2016-05-09',28),(42,'CZ','2016-05-09',29),(43,'CZ','2016-05-09',30),(44,'CZ','2016-05-11',1),(45,'CZ','2016-05-11',2),(46,'CZ','2016-05-11',3),(47,'CZ','2016-05-11',4),(48,'CZ','2016-05-11',5),(49,'CZ','2016-05-11',6),(50,'CZ','2016-05-11',7),(51,'CZ','2016-05-11',8),(52,'CZ','2016-05-11',9),(53,'CZ','2016-05-11',10),(54,'CZ','2016-05-11',11),(55,'CZ','2016-05-11',12),(56,'CZ','2016-05-11',13),(57,'CZ','2016-05-11',14),(58,'CZ','2016-05-11',15),(59,'CZ','2016-05-11',16),(60,'CZ','2016-05-11',17),(61,'CZ','2016-05-11',18),(62,'CZ','2016-05-11',19),(63,'CZ','2016-05-12',1),(64,'CZ','2016-05-12',2),(65,'CZ','2016-05-12',3);
 
 /*Table structure for table `tb_size` */
 
@@ -476,11 +474,11 @@ CREATE TABLE `tb_sizedtl` (
   `stat` varchar(10) DEFAULT '使用',
   `text` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 /*Data for the table `tb_sizedtl` */
 
-insert  into `tb_sizedtl`(`id`,`sizeId`,`name`,`code`,`stat`,`text`) values (8,8,'165S','001','使用','165S'),(9,8,'170','002','使用','170M'),(10,9,'180L','XXX','使用','180L'),(11,9,'175XX','XL','使用','175XX');
+insert  into `tb_sizedtl`(`id`,`sizeId`,`name`,`code`,`stat`,`text`) values (12,8,'size1','size1','1','size1'),(13,8,'size2','size2','1','size2');
 
 /*Table structure for table `tb_sku` */
 
@@ -491,19 +489,17 @@ CREATE TABLE `tb_sku` (
   `itemId` bigint(20) DEFAULT NULL,
   `colorId` bigint(20) DEFAULT NULL,
   `sizeDtlId` bigint(20) DEFAULT NULL,
-  `itemName` varchar(100) DEFAULT NULL,
-  `sizeDtlName` varchar(100) DEFAULT NULL,
-  `colorName` varchar(100) DEFAULT NULL,
+  `warehouseId` varchar(20) DEFAULT NULL,
   `text` varchar(20) DEFAULT NULL,
   `hasImage` tinyint(1) DEFAULT NULL,
   `imgSuffix` varchar(64) DEFAULT NULL,
   `comment` varchar(127) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 
 /*Data for the table `tb_sku` */
 
-insert  into `tb_sku`(`id`,`itemId`,`colorId`,`sizeDtlId`,`itemName`,`sizeDtlName`,`colorName`,`text`,`hasImage`,`imgSuffix`,`comment`) values (15,7,6,8,'a','165S','白','15',NULL,'.jpg',''),(16,7,6,10,'b','180L','白','16',NULL,'.jpg',''),(17,7,6,11,'17','175XX','白','17',NULL,'.png',''),(18,7,7,8,'18','165S','绿色','18',1,'.png','w'),(19,7,7,10,'19','180L','绿色','19',1,'.jpg','2134'),(20,7,7,11,'20','175XX','绿色','20',NULL,NULL,NULL),(21,5,6,8,'21','165S','白','21',NULL,NULL,NULL),(22,5,6,11,'22','175XX','白','22',NULL,NULL,NULL),(23,8,6,8,'234','165S','白','234',NULL,NULL,NULL),(24,9,6,8,'123','165S','白','123',NULL,NULL,NULL),(25,9,6,9,'123','170','白','123',NULL,NULL,NULL),(26,9,7,8,'123','165S','绿色','123',NULL,NULL,NULL),(27,9,7,9,'123','170','绿色','123',NULL,NULL,NULL);
+insert  into `tb_sku`(`id`,`itemId`,`colorId`,`sizeDtlId`,`warehouseId`,`text`,`hasImage`,`imgSuffix`,`comment`) values (28,10,6,12,NULL,'132',NULL,NULL,NULL),(29,10,6,13,NULL,'132',NULL,NULL,NULL),(30,10,7,12,NULL,'132',NULL,NULL,NULL),(31,10,7,13,NULL,'132',NULL,NULL,NULL);
 
 /*Table structure for table `tb_supplier` */
 
@@ -521,11 +517,11 @@ CREATE TABLE `tb_supplier` (
   `update_time` datetime DEFAULT NULL,
   `update_by` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 /*Data for the table `tb_supplier` */
 
-insert  into `tb_supplier`(`id`,`supplierName`,`supplierCode`,`mobile`,`type`,`stat`,`create_by`,`create_time`,`update_time`,`update_by`) values (1,'test001','test001','189999','供货','使用',NULL,NULL,'2016-05-08 09:28:30',NULL),(2,'test002','test002','189999','委外','停用',NULL,NULL,NULL,NULL),(3,'test003','test003','189999','委外','使用',NULL,NULL,'2016-05-08 09:28:36',NULL),(4,'test004','test004','189999','委外','停用',NULL,NULL,NULL,NULL),(5,'test005','test005','189999','委外','停用',NULL,NULL,NULL,NULL),(6,'test006','test006','189999','委外','停用',NULL,NULL,NULL,NULL),(7,'test007','test007','189999','委外','停用',NULL,NULL,NULL,NULL),(8,'test008','test008','189999','委外','停用',NULL,NULL,NULL,NULL),(10,'test009','test009','189999','委外','停用',NULL,NULL,NULL,NULL),(11,'test010','test010','189999','委外','停用',NULL,NULL,'2016-05-08 11:32:33',NULL),(12,'test011','test011','189999','委外','停用',NULL,NULL,NULL,NULL),(14,'45654','123','123','委外','使用',NULL,'2016-05-08 10:48:56','2016-05-08 10:49:11',NULL);
+insert  into `tb_supplier`(`id`,`supplierName`,`supplierCode`,`mobile`,`type`,`stat`,`create_by`,`create_time`,`update_time`,`update_by`) values (15,'顺丰','1','111','物流','使用',NULL,'2016-05-11 20:24:17',NULL,NULL),(16,'韵达','2','111','物流','使用',NULL,'2016-05-11 20:24:29',NULL,NULL);
 
 /*Table structure for table `tb_user` */
 
@@ -533,9 +529,9 @@ DROP TABLE IF EXISTS `tb_user`;
 
 CREATE TABLE `tb_user` (
   `id` bigint(16) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) DEFAULT NULL,
+  `user_name` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
-  `truename` varchar(50) DEFAULT NULL,
+  `true_name` varchar(50) DEFAULT NULL,
   `mobile` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `stat` varchar(10) DEFAULT NULL,
@@ -543,7 +539,7 @@ CREATE TABLE `tb_user` (
   `last_login_time` datetime DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_by` bigint(20) DEFAULT NULL,
-  `usercode` varchar(50) DEFAULT NULL,
+  `user_code` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='用户';
 
